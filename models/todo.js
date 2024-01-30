3e810ce74aa42a6f1af092a9b7b58258c6e8c3ec
Alioune Seck <aliouneseck15@gmail.com>
2024-01-30 17:56:53 -0500

    User & Todo models created

---
+++ models/todo.js
@@ -0,0 +1,13 @@
+const { model, Schema } = require('mongoose')
+
+const todoSchema = new Schema({
+    title: { type: String, required: true },
+    completed: { type: Boolean, required: true },
+    user: { type: Schema.Types.ObjectId, required: true, ref: 'User' }
+}, {
+    timestamps: true
+})
+
+const Todo = model('Todo', todoSchema)
+
+module.exports = Todo
---
+++ models/user.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypgt')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    todo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Todo' }]
+}, {
+    timestamps: true
+})
+
+userSchema.pre('save', async function(next) {
+    this.isModified('password')?
+    this.password = await bcrypt.hash(this.password, 8):
+    null;
+    next()
+})
+
+userSchema.methods.generateAuthToken = async function(){
+    const token = jwt.sign({ _id: this._id }, process.env.SECRET)
+    return token
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
