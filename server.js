1563dbbb364e7fa61b1a9418ebba261ff2f61779
Alioune Seck <aliouneseck15@gmail.com>
2024-01-30 22:18:48 -0500

    updated app & server.js; also added dev script dependency

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = require ('./app')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
-mongoose.connect(process.env.PORT)
-mongoose.connect.once('open', () => {
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
     console.log(`connected to mongo`)
 })
 
